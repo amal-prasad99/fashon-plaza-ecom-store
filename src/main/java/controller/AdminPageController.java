ad6f86d22cb70478f6b161acd01a6aa245d08f1a
FrostCafe <aps.project007@gmail.com>
2023-10-09 23:15:41 +0530

    Admin Page added

--- src/main/java/controller/AdminPageController.java
+++ src/main/java/controller/AdminPageController.java
@@ -1,4 +1,73 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AdminPageController {
+
+    @FXML
+    private JFXButton btnEmployee;
+
+    @FXML
+    private JFXButton btnItem;
+
+    @FXML
+    private JFXButton btnLogOut;
+
+    @FXML
+    private JFXButton btnOrd;
+
+    @FXML
+    private JFXButton btnOrderDetails;
+
+    @FXML
+    private JFXButton btnReturnSales;
+
+    @FXML
+    private JFXButton btnSalesReport;
+
+    @FXML
+    private JFXButton btnSupplier;
+
+    @FXML
+    void btnEmployeeOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnSalesOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalesReportOnActon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnActon(ActionEvent event) {
+
+    }
+
 }
