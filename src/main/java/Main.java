10c3dc52eb6262cd8f5f04fe93127adbeae7659e
FrostCafe <aps.project007@gmail.com>
2023-10-09 23:29:55 +0530

    Employee Page added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/AdminPagge.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Employee_page.fxml"))));
         primaryStage.show();
 
     }
