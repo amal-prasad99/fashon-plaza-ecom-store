bf3c16690cffaa77b41dd78841dddba3290043d7
FrostCafe <aps.project007@gmail.com>
2023-10-09 22:26:52 +0530

    LogIn interface added

--- src/main/java/controller/LogInPageController.java
+++ src/main/java/controller/LogInPageController.java
@@ -1,4 +1,51 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class LogInPageController {
+
+    @FXML
+    private JFXButton btnLogIn;
+
+    @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
+    private CheckBox chkShowPassword;
+
+    @FXML
+    private Label lblForgotPassword;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLogInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chkShowPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lblForgotPasswordOnAction(MouseEvent event) {
+
+    }
+
 }
