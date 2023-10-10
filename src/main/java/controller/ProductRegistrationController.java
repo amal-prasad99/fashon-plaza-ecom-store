06b0fa4f2905d75a530f56348d9262e8ee70c86f
FrostCafe <aps.project007@gmail.com>
2023-10-10 14:08:18 +0530

    Product Registration Page added

---
+++ src/main/java/controller/ProductRegistrationController.java
@@ -0,0 +1,78 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ProductRegistrationController {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnPrint;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXComboBox<?> cmbProductCatagory;
+
+    @FXML
+    private Label lblProductId;
+
+    @FXML
+    private JFXTextField txtProductDiscription;
+
+    @FXML
+    private JFXTextField txtProductName;
+
+    @FXML
+    private JFXTextField txtProductSearch;
+
+    @FXML
+    private JFXTextField txtProductSize;
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPrintOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+}
