ab55e6baf3c897954b0eceeb9e606b773dac0abb
FrostCafe <aps.project007@gmail.com>
2023-10-10 13:57:29 +0530

    Supplier Registration Page added

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,17 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+w
+G.idea/sonarlint/issuestore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,e\d\ed67eb178d523a4b3f117dcbe0eda5196367ceec
+Å
+Q.idea/sonarlint/securityhotspotstore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,3\0\30205a209c6ce77c4cfc19a09e6f57c588b7fb88
+R
+"src/main/java/db/DBConnection.java,f\d\fd7336f68247f5ace91042607378ff616e4285dd
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+a
+1src/main/java/controller/LogInPageController.java,a\4\a459e3f0cdd04e83f36792817ad4537c38d3df74
+a
+1src/main/java/controller/AdminPageController.java,2\c\2c4bf40b863ab13553db23bdf41086fbbbaa94f4
+d
+4src/main/java/controller/EmployeePageController.java,e\3\e3c01f7d3225193dc440d95aa738d91c40e277d3
---
+++ src/main/java/controller/SupplierRegistrationController.java
@@ -0,0 +1,78 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SupplierRegistrationController {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnPrint;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXComboBox<?> cmbSupplierTitle;
+
+    @FXML
+    private Label lblSupplierID;
+
+    @FXML
+    private JFXTextField txtSupplierCompany;
+
+    @FXML
+    private JFXTextField txtSupplierEmail;
+
+    @FXML
+    private JFXTextField txtSupplierName;
+
+    @FXML
+    private JFXTextField txtSupplierSearch;
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPrintOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+}
