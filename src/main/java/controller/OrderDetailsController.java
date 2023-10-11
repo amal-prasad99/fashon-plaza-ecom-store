35caf8c03db173974a1c3b8cfad8ce0428c85e5d
FrostCafe <aps.project007@gmail.com>
2023-10-11 11:29:40 +0530

    Order Details Page added

---
+++ src/main/java/controller/OrderDetailsController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class OrderDetailsController {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnReturn;
+
+    @FXML
+    private JFXTextField txtOrderID;
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+}
